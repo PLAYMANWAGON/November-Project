847d3ce09b41a3d1e260e5230d25406af182e94e
Aironas <aironas.keturka@elev.ga.ntig.se>
2021-01-18 08:47:52 +0100

    Basically hela programmet

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Runtime.ConstrainedExecution;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Collections;
+using System.ComponentModel;
+using System;
 using Raylib_cs;
 
 namespace RPG
@@ -13,9 +18,9 @@ namespace RPG
 
             Random generator = new Random();
 
-            //-----STRINGS-----
+            #region strings
 
-            //GAME SCENE THINGY
+            //GAME SCENE
 
             string scene = "titleScreen";
 
@@ -27,7 +32,9 @@ namespace RPG
 
             string[] text = { "Dialogue 1", "Dialogue 2" };
 
-            //-----FLOATS-----
+            #endregion
+
+            #region floats
 
             //CHARACTER STARTING POSITION
 
@@ -35,43 +42,54 @@ namespace RPG
 
             float yPos = 284;
 
-            //-----INTEGERS-----
+            //AUDIO SETTINGS
+
+            float musicVolume = 1f;
+
+            float afxVolume = 1f;
+            
+            string muVolString = musicVolume.ToString();
+
+            string afxVolString = afxVolume.ToString();
+
+            #endregion
+
+            #region integers
 
             //THE SHITTY INTEGER THAT MAKES THE MENU WORK (IF IT WORKS, IT WORKS)
 
             int menu = 0;
 
+            int optionMenu = 0;
+
+            int battleMenu = 0;
+
             //WIDTH AND HEIGHT INTEGERS IF I NEED TO PLACE SOMETHING IN SOME POSITION
 
             int width = 800;
 
-            int height = 600;
 
-            //CollisionDetection Square
+            //HEALTH FOR BATTLE
+
+            int pHealth = 100;
+
+            int eHealth = 100;
+
+            #endregion
+
+            //COLLISION SQUARE
 
             Rectangle colRect = new Rectangle(590, 200, 100, 100);
 
             Rectangle colRect2 = new Rectangle(200, 250, 50, 50);
 
-
+            #region Textures
 
             //-----IMAGES AND TEXTURES AND SCENES-----
 
-
-            //CHARACTER TEXTURE - INSIDE
-
-            Image charImg = Raylib.LoadImage(@"still.png");
-
-            Texture2D stand = Raylib.LoadTextureFromImage(charImg);
-
-
-
-            //CHARACTER TEXTURE - WORLD
-
-
             #region Character Textures
 
-            #region north
+            //NORTH----------------------------------------------------------
 
             Image charLoN = Raylib.LoadImage(@"worldN.png");
 
@@ -79,44 +97,40 @@ namespace RPG
 
             Texture2D worldN = Raylib.LoadTextureFromImage(charLoN);
 
-            #endregion
 
-            #region south 
+            //SOUTH-----------------------------------------------------------
+
             Image charLoS = Raylib.LoadImage(@"worldS.png");
 
             Raylib.ImageResizeNN(ref charLoS, 64, 64);
 
             Texture2D worldS = Raylib.LoadTextureFromImage(charLoS);
 
-            #endregion
+            //EAST------------------------------------------------------------
 
-            #region east
             Image charLoE = Raylib.LoadImage(@"worldE.png");
 
             Raylib.ImageResizeNN(ref charLoE, 64, 64);
 
             Texture2D worldE = Raylib.LoadTextureFromImage(charLoE);
 
-            #endregion
+            //WEST------------------------------------------------------------
 
-            #region west
             Image charLoW = Raylib.LoadImage(@"worldW.png");
 
             Raylib.ImageResizeNN(ref charLoW, 64, 64);
 
             Texture2D worldW = Raylib.LoadTextureFromImage(charLoW);
 
-            #endregion
 
 
             #endregion
 
+            #region Scene Textures
 
+            #region scene 1
 
-            //SCENE 1 TEXTURES
-
-
-            //SCENERY
+            //SCENERY-1----------------------------------------------------------------------
 
             Image scene1Img = Raylib.LoadImage(@"scene1.png");
 
@@ -124,35 +138,75 @@ namespace RPG
 
             Texture2D scene1Tex = Raylib.LoadTextureFromImage(scene1Img);
 
-            //SCENERY OBJECTS
 
-            //TOP LAYER
+            //SCENERY 1 OBJECTS
+
+            //TOP LAYER----------------------------------------------------------------------
+
             Image scene1ObjLyr1Img = Raylib.LoadImage(@"scene1ObjLyr1.png");
 
             Raylib.ImageResizeNN(ref scene1ObjLyr1Img, 800, 600);
 
             Texture2D scene1ObjLyr1Tex = Raylib.LoadTextureFromImage(scene1ObjLyr1Img);
 
-            //BOTTOM LAYER
+            //BOTTOM LAYER--------------------------------------------------------------------
+
             Image scene1ObjLyr2Img = Raylib.LoadImage(@"scene1ObjLyr2.png");
 
             Raylib.ImageResizeNN(ref scene1ObjLyr2Img, 800, 600);
 
             Texture2D scene1ObjLyr2Tex = Raylib.LoadTextureFromImage(scene1ObjLyr2Img);
+            //--------------------------------------------------------------------------------
+
+            #endregion
+
+            #region fight scene
+
+            Image battleImg = Raylib.LoadImage(@"battlesketch.png");
+
+            Texture2D battleTex = Raylib.LoadTextureFromImage(battleImg);
+
+            Image winImg = Raylib.LoadImage(@"winning.jpg");
+
+            Texture2D winTex = Raylib.LoadTextureFromImage(winImg);
 
 
+            #endregion
 
 
+            #endregion
 
+            #endregion
 
+            #region Audio
+            
+            Raylib.InitAudioDevice();
+
+            Music menuMusic = Raylib.LoadMusicStream(@"MenuMusic.mp3");
+
+            Raylib.PlayMusicStream(menuMusic);
+
+            Sound menuEnter = Raylib.LoadSound(@"MenuEnter.wav");
+
+            Sound menuExit = Raylib.LoadSound(@"MenuExit.wav");
+
+            Sound menuSelect = Raylib.LoadSound(@"MenuSelect.wav");
+
+            //GAME AUDIO------------------------------------------------------
+
+            Music gameMusic = Raylib.LoadMusicStream(@"GameMusic.mp3");
+
+            Raylib.PlayMusicStream(gameMusic);
+
+            //BATTLE AUDIO----------------------------------------------------
+
+            Music battleMusic = Raylib.LoadMusicStream(@"fight.mp3");
+
+            Raylib.PlayMusicStream(battleMusic);
+
+            #endregion
 
             Raylib.SetTargetFPS(60);
-
-
-
-
-
-
 
 
             while (!Raylib.WindowShouldClose())
@@ -167,9 +221,12 @@ namespace RPG
                 bool areOverlapping2 = Raylib.CheckCollisionRecs(colRect2, playRect);
 
 
-
                 if (scene == "game")
                 {
+
+                    Raylib.UpdateMusicStream(gameMusic);
+
+                    Raylib.PlayMusicStream(gameMusic);
 
                     //SCENE AND BOTTOM TEXTURES
 
@@ -184,20 +241,22 @@ namespace RPG
                     bool isMoving = false;
 
 
-                    //DISPLAYED (INVISIBLE) COLLISION BOXES
+                    #region CollisionBoxes
 
-                    Raylib.DrawRectangleRec(colRect, Color.WHITE);
+                    Raylib.DrawRectangleRec(colRect, Color.BLANK);
 
-                    Raylib.DrawRectangleRec(playRect, Color.WHITE);
+                    Raylib.DrawRectangleRec(playRect, Color.BLANK);
 
-                    Raylib.DrawRectangleRec(colRect2, Color.WHITE);
+                    Raylib.DrawRectangleRec(colRect2, Color.BLANK);
 
+                    #endregion
+
+
+                    //MOVEMENT, DIRECTION, SCARY MONSTERS AND NICE SPRITES AND CRAP 
 
                     float xMovement = 0;
                     float yMovement = 0;
-
-                    //MOVEMENT, DIRECTION, SCARY MONSTERS AND NICE SPRITES AND SOME OTHER CRAP 
-
+                    
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
                     {
                         facing = "east";
@@ -254,6 +313,11 @@ namespace RPG
                         xPos -= xMovement;
                     }
 
+                    if (Raylib.CheckCollisionRecs(playRect, colRect2))
+                    {
+                        xPos += xMovement;
+                    }
+
 
 
                     //THE CODE THAT REMOVES GHOSTING (OTHERWISE TWO SPRITES ARE DISPLAYED AT THE SAME TIME WHILE MOVING)
@@ -291,11 +355,12 @@ namespace RPG
 
                     }
 
-                    if (areOverlapping == true)
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_B)) 
                     {
 
+                        scene = "battle";
 
-
+                        
                     }
 
                     //THE OTHER TEXTURE THAT MAKES CHARACTER STAY BEHIND OBJECTS
@@ -311,13 +376,226 @@ namespace RPG
 
                 }
 
+                if (scene == "battle")
+                {
+                    Raylib.UpdateMusicStream(battleMusic);
+
+                    Raylib.PlayMusicStream(battleMusic);
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN))
+                    {
+
+                        Raylib.PlaySound(menuSelect);
+
+                        battleMenu++;
+
+                    }
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+                    {
+
+                        Raylib.PlaySound(menuSelect);
+
+                        battleMenu--;
+
+                    }
+
+                    if (battleMenu > 1)
+                    {
+
+                        battleMenu = 1;
+                    }
+
+                    if (battleMenu < 0)
+                    {
+
+                        battleMenu = 0;
+                    }
+
+                    if (battleMenu == 0 && Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+                        int dmg = generator.Next(51);
+
+                        eHealth = eHealth - DamageMaker.StrikeDamage(dmg);
+
+                            
+                    }
+
+                    if (battleMenu == 1 && Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+                        int dmg = generator.Next(10, 61);
+
+                        eHealth = eHealth - DamageMaker.MagicDamage(dmg);
+                    }
+
+                    if (eHealth <= 0)
+                    {
+
+                    scene = "win";
+
+                    }
+
+                    Raylib.BeginDrawing();
+
+                    Raylib.DrawText("Strike", 50, 450, 26, Color.BLACK);
+                    Raylib.DrawText("Magic", 50, 500, 26, Color.BLACK);
+                    Raylib.DrawText("Enemy Health: " + eHealth + " /100", 350, 100, 36, Color.BLACK);
 
 
+                    if (battleMenu == 0)
+                    {
+                        Raylib.DrawText("<", 150, 450, 32, Color.BLACK);
+                    }
+
+                    if (battleMenu == 1)
+                    {
+                        Raylib.DrawText("<", 150, 500, 32, Color.BLACK);
+                    }
+
+
+
+                    Raylib.ClearBackground(Color.WHITE);
+
+                    Raylib.EndDrawing();
+
+                }
+
+                if (scene == "win")
+                {
+                    Raylib.BeginDrawing();
+
+                    Raylib.DrawText("Texturer har slutat fungera så \n det här får vara min win \n screen :/", 20, 20, 36, Color.MAGENTA);
+
+                    Raylib.ClearBackground(Color.WHITE);
+
+                    Raylib.EndDrawing();
+                }
+
+                if (scene == "options")
+                {
+
+                    Raylib.UpdateMusicStream(menuMusic);
+
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN))
+                    {
+
+                        Raylib.PlaySound(menuSelect);
+
+                        optionMenu++;
+
+                    }
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+                    {
+
+                        Raylib.PlaySound(menuSelect);
+
+                        optionMenu--;
+
+                    }
+
+
+                    Raylib.BeginDrawing();
+
+                    if (optionMenu == 0)
+                    {
+
+                        Raylib.DrawText("<", 600, 300, 32, Color.WHITE);
+
+                    }
+
+                    if (optionMenu == 1)
+                    {
+
+                        Raylib.DrawText("<", 600, 350, 32, Color.WHITE);
+
+                    }
+
+                    if (optionMenu == 2)
+                    {
+
+                        Raylib.DrawText("<", 500, 400, 32, Color.WHITE);
+
+                    }
+
+                    if (optionMenu == 0 && Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))
+                    {
+
+                        musicVolume = musicVolume -= 0.1f;
+
+                        
+                    }
+
+                    if (optionMenu == 1 && Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))
+                    {
+
+                        afxVolume = afxVolume - 0.1f;
+
+                        
+                    }
+
+                    if (optionMenu == 2 && Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+
+                        Raylib.PlaySound(menuExit);
+
+                        scene = "titleScreen";
+                    }
+
+                    if (optionMenu > 2)
+                    {
+
+                        optionMenu = 2;
+                    }
+
+                    if (optionMenu < 0)
+                    {
+
+                        optionMenu = 0;
+                    }
+
+                    
+
+                    Raylib.ClearBackground(Color.BLUE);
+
+                    Raylib.DrawText("Options", 200, 100, 128, Color.WHITE);
+
+                    Raylib.DrawText("Music Volume", width / 4 , 300, 32, Color.WHITE);
+                    Raylib.DrawText(muVolString, 500, 300 , 32, Color.MAGENTA);
+
+                    Raylib.DrawText("AFX Volume", width / 4, 350, 32, Color.WHITE);
+                    Raylib.DrawText(afxVolString, 500, 350 , 32, Color.MAGENTA);
+
+
+                    Raylib.DrawText("Back", width / 2 - 40, 400, 32, Color.WHITE);
+
+                    
+
+
+                    Raylib.EndDrawing();
+                }
 
                 if (scene == "titleScreen")
                 {
+                    Raylib.SetMusicVolume(menuMusic, musicVolume);
+
+                    Raylib.UpdateMusicStream(menuMusic);
+
+                    Raylib.PlayMusicStream(menuMusic);
+
+                    Raylib.SetSoundVolume(menuEnter, afxVolume);
+                    Raylib.SetSoundVolume(menuExit, afxVolume);
+                    Raylib.SetSoundVolume(menuSelect, afxVolume);
+
+                    Raylib.SetMusicVolume(menuMusic, musicVolume);
+                    Raylib.SetMusicVolume(gameMusic, musicVolume);
+
+
                     if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN))
                     {
+
+                        Raylib.PlaySound(menuSelect);
 
                         menu++;
 
@@ -325,6 +603,8 @@ namespace RPG
 
                     if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
                     {
+
+                        Raylib.PlaySound(menuSelect);
 
                         menu--;
 
@@ -357,7 +637,27 @@ namespace RPG
                     if (menu == 0 && Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
                     {
 
+                        Raylib.PlaySound(menuEnter);
+
                         scene = "game";
+                    }
+
+                    if (menu == 1 && Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+
+                        Raylib.PlaySound(menuEnter);
+
+                        scene = "options";
+                    }
+
+                    if (menu == 2 && Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+
+                        Raylib.PlaySound(menuExit);
+
+                        Raylib.CloseWindow();
+
+                        return;
                     }
 
                     if (menu > 2)
@@ -380,20 +680,26 @@ namespace RPG
                     Raylib.DrawText("Options", width / 3 + 20, 350, 32, Color.WHITE);
                     Raylib.DrawText("Quit", width / 3 + 20, 400, 32, Color.WHITE);
 
-
-
-
-
                     Raylib.EndDrawing();
                 }
 
-
-
-
-
             }
 
+        }
 
+    }
+
+    class DamageMaker
+    {
+        public static int StrikeDamage(int damage)
+        {
+            return damage % 15;
+        }
+
+        public static int MagicDamage(int magic)
+        {
+            return magic % 15 * 2;
         }
     }
+
 }
